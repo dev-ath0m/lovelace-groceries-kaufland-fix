ffcdb0918607678c9689b6bc4b521cd297f5bdad
dev-ath0m <artjom.schwabski@googlemail.com>
2026-09-20 14:31:14 +0200

    Prefer canonical valid_until for offer due dates

--- src/utils.js
+++ src/utils.js
@@ -172,12 +172,12 @@ export function normalizeOffer(item, storeEntity, hass = null, storeConf = null)
     item.startDate ||
     '';
   const dateTo =
+    item.valid_until ||
+    item.validUntil ||
     item.end_date ||
     item.endDate ||
     item.date_to ||
     item.dateTo ||
-    item.valid_until ||
-    item.validUntil ||
     item.valid_to ||
     item.validTo ||
     item.untilDate ||
