2741cfb8cd9711d8ecc21fcddfe9241ad9507ab3
dev-ath0m <249173984+dev-ath0m@users.noreply.github.com>
2026-09-20 22:12:58 +0200

    Prepare v0.1.11-beta.1 Todo API fix

--- src/card.js
+++ src/card.js
@@ -836,7 +836,7 @@ class DiscountsCard extends HTMLElement {
   }
 }
 
-const CARD_VERSION = '0.1.10';
+const CARD_VERSION = '0.1.11-beta.1';
 console.info(
   `%c DISCOUNTS-CARD (KAUFLAND FIX) %c v${CARD_VERSION} `,
   'color: white; background: #039be5; font-weight: 700;',
